25d9ef78601c46f6f2232a248720ae4463f72aaf
christianquebral <christianquebral@gmail.com>
2022-01-28 00:01:09 -0800

    added ssl/cors

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Resource, Api
 from models import PlayerDetail
 
@@ -9,16 +10,26 @@ from resources.storeitem import StoreItems
 from resources.purchase import Purchases
 from custom import Leaderboard, Player, PlayerInventory
 
+import ssl  
+
+
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
+
+if ENV == "prod":
+    ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    ctx.load_cert_chain("./cert/fullchain.pem", "./cert/privkey.pem")
+else:
+	pass
 
 api.add_resource(Player, "/player/<string:player_name>")
 api.add_resource(PlayerAuth, "/player/auth")
 api.add_resource(PlayerInventory, "/player/<string:player_name>/inventory")
 api.add_resource(PlayerStates, "/player/states")
 api.add_resource(Games, "/games")
+api.add_resource(Leaderboard, "/leaderboard")
 api.add_resource(Purchases, "/store/purchase")
 api.add_resource(StoreItems, "/store")
-api.add_resource(Leaderboard, "/leaderboard")
 
 app.run(host="0.0.0.0", port=80, debug=True)
